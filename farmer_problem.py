2c446f0a430bba871784e93bb103f2adb9f5843c
LucHighwalker <luc.highwalker@gmail.com>
2019-06-19 16:09:00 -0700

    Basic brute force approach to farmer problem

--- farmer_problem.py
+++ farmer_problem.py
@@ -1,5 +1,27 @@
-def optimize_farm(total_acres, total_hours, corn_profit, oats_profit, corn_hours, oats_hours):
-    pass
+def optimize_farm(total_acres, total_hours, corn_profit, oats_profit, corn_hours, oats_hours, accuracy=0.01):
+    optimized = False
+    divider = accuracy
+
+    while not optimized and divider <= 1:
+      corn_acres = divider * total_acres
+      oats_acres = total_acres - corn_acres
+
+      corn_time = corn_acres * corn_hours
+      oats_time = oats_acres * oats_hours
+
+      total_time = corn_time + oats_time
+
+      if total_time >= total_hours:
+        optimized = True
+      else:
+        total_corn_profit = corn_acres * corn_profit
+        total_oats_profit = oats_acres * oats_profit
+        total_profit = total_corn_profit + total_oats_profit
+        
+      divider += accuracy
+
+    return (round(total_profit, 2), round(corn_acres, 2), round(oats_acres, 2))
+
 
 
 if __name__ == "__main__":
